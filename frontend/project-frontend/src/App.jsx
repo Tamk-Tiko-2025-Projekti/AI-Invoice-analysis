1ad17e3a1eb7d21b51ee687d00f2039642888c71
Mika Kuusisto <mkuusisto00@gmail.com>
2025-03-24 15:07:15 +0200

    feat(frontend): Added file upload. Uses axios for temp testing

--- frontend/project-frontend/src/App.jsx
+++ frontend/project-frontend/src/App.jsx
@@ -1,33 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import FileSelection from './FileUploader'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>To do:</h1>
+        <ul>
+          <li>Make fileuploader upload to our backend</li>
+          <li>Make another page to fetch the data from backend and showcase it</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <FileSelection/>
     </>
   )
 }
---
+++ frontend/project-frontend/src/FileUploader.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+// At the moment uses axios to simulate uploading file to a backend.
+const FileUploader = () => {
+  const [file, setFile] = useState(null)
+  const [status, setStatus] = useState("idle")
+
+  function handleFileChange(e) {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  }
+
+  async function handleFileUpload() {
+    if (!file) return;
+
+    setStatus("uploading")
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      await axios.post("https://httpbin.org/post", formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      setStatus("success");
+    } catch {
+      setStatus("error");
+    }
+  }
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      {file && (
+        <div>
+          <p>File name: {file.name}</p>
+          <p>Type: {file.type}</p>
+        </div>
+      )}
+
+      {file && status !== "uploading" && (
+        <button onClick={handleFileUpload}>Upload</button>
+      )}
+
+      {status === "uploading" && (
+        <p>File is uploading...</p>
+      )}
+
+      {status === "success" && (
+        <p>File uploaded succesfully!</p>
+      )}
+
+      {status === "error" && (
+        <p>Error uploading the file</p>
+      )}
+    </div>
+  );
+}
+
+export default FileUploader;
