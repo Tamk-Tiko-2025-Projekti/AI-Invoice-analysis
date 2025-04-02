3f4ce8a06abe756814365418f4a44b2809b7caaf
Nitaivs <144683740+Nitaivs@users.noreply.github.com>
2025-04-02 21:07:17 +0300

    feat: add command-line argument support for PDF file path in convertpdf.py

--- spring_back/App/src/main/kotlin/fi/project/app/util/convertpdf.py
+++ spring_back/App/src/main/kotlin/fi/project/app/util/convertpdf.py
@@ -1,6 +1,13 @@
+import sys
+
 from pdf2image import convert_from_path
 
-image = convert_from_path('./temp/test.pdf', dpi=90, fmt="webp")
+for i in range(1, len(sys.argv)):
+    print(sys.argv[i])
 
-for i in range(len(image)):
-    image[i].save('./temp/test' + str(i) + '.webp', 'webp')
+if (len(sys.argv) < 2):
+    print("Please provide a PDF file path.")
+    sys.exit(1)
+
+image = convert_from_path('./temp/temp.pdf', dpi=90, fmt="webp")
+image[0].save('./temp/temp' + '.webp', 'webp')
