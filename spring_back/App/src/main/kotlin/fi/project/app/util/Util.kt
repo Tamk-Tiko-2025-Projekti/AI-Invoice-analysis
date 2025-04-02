913b8938e5160150fc9a5176d2ddf11958f52c21
Nitaivs <144683740+Nitaivs@users.noreply.github.com>
2025-04-02 20:17:26 +0300

    feat: update saveFile function to use absolute path and save with proper extension

--- spring_back/App/src/main/kotlin/fi/project/app/util/Util.kt
+++ spring_back/App/src/main/kotlin/fi/project/app/util/Util.kt
@@ -12,15 +12,22 @@ fun convertToPDF() {
 }
 
 fun saveFile(file: MultipartFile, path: String): File {
+    // Get the current working directory and append the given path
+    var newPath = System.getProperty("user.dir") + path
+    newPath = newPath.replace("\\", "/")
+    println("New path: $newPath")
     // Create the directory if it doesn't exist
-    val directory = File(path)
+    val directory = File(newPath)
     if (!directory.exists()) {
         directory.mkdir()
     }
 
     /* The file is saved in the project root directory. */
-    val savedFile = File(path, file.originalFilename ?: "tempfile")
+//    val savedFile = File(newPath, file.originalFilename ?: "tempfile")
+    val fileExtension = file.originalFilename?.substringAfterLast(".")
+    val savedFile = File(newPath, "temp.$fileExtension")
     file.transferTo(savedFile)
+    println("File saved to: ${savedFile.absolutePath}")
 
     return savedFile
 }
