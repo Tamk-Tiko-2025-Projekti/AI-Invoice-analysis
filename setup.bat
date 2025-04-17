34fa016bc7818e8e99e817cda24506cf1d430e4c
Nitaivs <144683740+Nitaivs@users.noreply.github.com>
2025-04-17 19:12:52 +0300

    feat(start.bat): check if virtual environment exists and create it if not

--- setup.bat
+++ setup.bat
@@ -1,10 +1,18 @@
+@echo off
 pushd frontend
+echo "Installing frontend dependencies..."
 call npm install
+echo "Building frontend..."
 call npm run build
 popd
 xcopy frontend\dist\* spring_back\App\src\main\resources\static /E /I /Y
 pushd spring_back\App
+IF NOT EXIST "venv" (
+    echo "Creating python virtual environment..."
+    python -m venv venv
+)
 call venv\Scripts\activate
+echo "Installing python dependencies..."
 call pip install -r requirements.txt
 call deactivate
 popd
